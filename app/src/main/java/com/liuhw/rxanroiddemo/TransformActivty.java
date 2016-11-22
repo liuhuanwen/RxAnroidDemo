5bbec9b765d7264b9aa5672cea3cadcea34b20a5
liuhw <liuhuansir@126.com>
2016-11-22 22:03:18 +0800

    Transforming类型操作符

---
+++ app/src/main/java/com/liuhw/rxanroiddemo/TransformActivty.java
@@ -0,0 +1,13 @@
+package com.liuhw.rxanroiddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransformActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transform);
+    }
+}
